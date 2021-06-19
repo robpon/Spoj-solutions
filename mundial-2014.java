60d5a2f55cb7000b9150dca934c769393aeac444
robpon <86128798+robpon@users.noreply.github.com>
2021-06-19 10:14:42 +0200

    Create mundial-2014.java

---
+++ mundial-2014.java
@@ -0,0 +1,15 @@
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String a="mu";
+String b="";
+String c ="a";
+String d="abcd";
+System.out.print("Mundial ");
+System.out.print(a.length());
+        System.out.print(b.length());
+        System.out.print(c.length());
+        System.out.print(d.length());
+	}
+}
