f1e4273e9b951ac6f817ffaa3f8460444bb7be04
robpon <86128798+robpon@users.noreply.github.com>
2021-06-19 10:57:53 +0200

    Create zliczanie-linii.java

---
+++ zliczanie-linii.java
@@ -0,0 +1,18 @@
+  import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws java.lang.Exception {
+  
+   BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+String c;
+int i=0;
+        while ((c = r.readLine()) != null) {
+            i++;
+        }
+                    System.out.println(i);
+
+		
+	}
+}
